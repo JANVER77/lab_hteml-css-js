0be0cac9f494b8be0240662d080017267bb7eb2b
EdwardR <janver77@gmail.com>
2025-11-06 15:31:02 -0500

    update menos

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,20 @@ document.getElementById("btn").addEventListener('click', function(e){
         li.textContent = num;
         num += 1;
     document.getElementById("contador-list").appendChild(li);
-})
+
+
+});
+
+document.getElementById("btn2").addEventListener('click', function(e){
+e.preventDefault();
+const li = document.createElement("li");
+    li.textContent = num;
+    num -= 1;
+document.getElementById("contador-list").appendChild(li);
+
+});
+
+
 
 
 
